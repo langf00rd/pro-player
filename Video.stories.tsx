9e09dbbf6195026dbf8a1b528c8a35d501bb5792
Langford Kwabena <55645613+langford-dev@users.noreply.github.com>
2023-07-24 12:35:54 +0000

    v2 files

---
+++ Video.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Video from "./Video";
+
+const meta: Meta<typeof Video> = {
+  component: Video,
+};
+
+export default meta;
+type Story = StoryObj<typeof Video>;
+
+export const Default: Story = {
+  render: ({ ...props }) => (
+    <Video
+      {...props}
+      controls
+      src="https://live-par-2-abr.livepush.io/vod/bigbuckbunny/index.m3u8"
+    />
+  ),
+};
+
+// https://live-par-2-abr.livepush.io/vod/bigbuckbunny/index.m3u8
+// https://live-par-1-abr-cdn.livepush.io/live_abr_cdn/emaIqCGoZw-6/index.m3u8
